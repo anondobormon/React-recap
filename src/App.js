3b2aab47286266681e1d3fc54c4611f1815e4f12
anondobormon <anondokpi@gmail.com>
2021-02-25 01:01:23 +0600

    recap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Learning React</p>
+        <Users></Users>
+        <div>
+          {
+            users.map(user => <Users name={user.name} email={user.email}></Users>)
+          }
+        </div>
+
       </header>
     </div>
   );
 }
+
+function Users(props) {
+
+  return (
+    <div style={productStyle}>
+      <h3>This is the recap session</h3>
+      <h3>{props.name}</h3>
+      <p>{props.email}</p>
+    </div>
+  )
+}
+
+let productStyle = {
+  boxSizing: 'border-box',
+  border: '2px solid black',
+  borderRadius:'5px',
+  backgroundColor: 'lightgray',
+  height: '300px',
+  width: '400px',
+  color: 'black',
+  margin: '10px'
+}
+
+
+
 
 export default App;
